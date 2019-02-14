d8c1c563890ad6aa1dcc3034c2b4a37f9143f11e
Gerardas Martynovas <gerardas@8devices.com>
2019-02-14 22:09:40 +0200

    add initial argparser

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+#!/usr/bin/python3
+
+
+import argparse
+
+
 def main():
-    print('Hello world!')
+    parser = argparse.ArgumentParser(description='Generate graphs and search through them.')
+    subparsers = parser.add_subparsers(help='sub-command help')
+    generate_parser = subparsers.add_parser('generate', help='generate graph from given data')
+    generate_parser.add_argument('-v', type=int, metavar='<count>', required=True, help='amount of verticles')
+    generate_parser.add_argument('-n', type=int, metavar='<count>', required=True, help='minimum amount of neighbours')
+    generate_parser.add_argument('-x', type=int, metavar='<count>', required=True, help='maximum amount of neighbours')
 
-if __name__ = '__main__':
+    draw_parser = subparsers.add_parser('draw', help='draw current graph')
+    search_parser = subparsers.add_parser('search', help='search in current graph')
+
+    args = parser.parse_args()
+
+
+if __name__ == '__main__':
     main()
-
