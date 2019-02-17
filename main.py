7f22b4e5b62797ca094a6f24a45723f15e914ad1
Gerardas Martynovas <gerardas@8devices.com>
2019-02-17 02:04:56 +0200

    make graph generation timer togglable

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def read_args():
     generate_parser.add_argument('-v', type=int, metavar='<count>', required=True, help='amount of vertices')
     generate_parser.add_argument('-n', type=int, metavar='<count>', required=True, help='minimum amount of neighbours')
     generate_parser.add_argument('-x', type=int, metavar='<count>', required=True, help='maximum amount of neighbours')
+    generate_parser.add_argument('-t', action='store_true', help='show generation time')
 
     draw_parser = subparsers.add_parser('draw', help='draw current graph')
     draw_parser.add_argument('-m', type=str, metavar='<name>', required=True, help='name of graph')
@@ -33,7 +34,8 @@ def main():
     try:
         if args.v and args.n and args.x:
             graph = Graph(args.v, args.n, args.x)
-            print('Graph generated in {} seconds.'.format(graph.graph_generation_time))
+            if args.t:
+                print('Graph generated in {} seconds.'.format(graph.graph_generation_time))
             return
         elif args.m:
             Graph().draw()
